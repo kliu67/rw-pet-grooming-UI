1517c4e489e49716b6661109de2f29baf8025953
kliu67 <47793951+kliu67@users.noreply.github.com>
2026-02-26 10:21:21 -0500

    clean upi modalregistry

--- src/components/modal/modalRegistry.js
+++ src/components/modal/modalRegistry.js
@@ -1,15 +1,9 @@
-import CreateServiceModal from "./types/CreateServiceModal";
-import EditServiceModal from "./types/EditServiceModal";
 import DeleteModal from "./types/DeleteModal";
 
 export const MODAL_TYPES = {
-  CREATE_SERVICE: "CREATE_SERVICE",
-  EDIT_SERVICE: "EDIT_SERVICE",
   DELETE: "DELETE"
 };
 
 export const MODAL_COMPONENTS = {
-  [MODAL_TYPES.CREATE_SERVICE]: CreateServiceModal,
-  [MODAL_TYPES.EDIT_SERVICE]: EditServiceModal,
   [MODAL_TYPES.DELETE]: DeleteModal
 };
