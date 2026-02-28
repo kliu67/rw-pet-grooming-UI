8dfe168cc1951c9329ebfb92dce913822bc67cb4
kliu67 <47793951+kliu67@users.noreply.github.com>
2026-02-28 10:31:57 -0500

    Add constants

--- src/constants.js
+++ src/constants.js
@@ -5,11 +5,20 @@ export const MIN_SERVICE_BASE_PRICE_VALUE = 0;
 export const MAX_SERVICE_BASE_PRICE_VALUE = 99999999.99;
 
 //breed constants
+export const BREEDS_QUERY_KEY = "breeds";
 export const MAX_BREED_NAME_LENGTH = 60;
 
 //client constants
+export const CLIENTS_QUERY_KEY = "clients";
 export const MAX_CLIENT_FIRST_NAME_LENGTH = 60;
 export const MAX_CLIENT_LAST_NAME_LENGTH = 60;
 export const MAX_CLIENT_EMAIL_LENGTH = 60;
 export const MAX_CLIENT_PHONE_LENGTH = 20;
 export const MAX_CLIENT_DESC_LENGTH = 200;
+
+//pets
+export const MAX_PET_NAME_LENGTH = 60;
+export const PETS_QUERY_KEY = "pets";
+
+//weight classes
+export const WEIGHT_CLASSES_QUERY_KEY = "weightClasses";
