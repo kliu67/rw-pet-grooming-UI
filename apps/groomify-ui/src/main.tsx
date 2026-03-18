5185062dac2ce459e9f8406ab15e7a6ed66a7dc3
KL <>
2026-03-18 14:30:47 -0400

    add i18n

--- apps/groomify-ui/src/main.tsx
+++ apps/groomify-ui/src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
+import "./i18n";
 
 const queryClient = new QueryClient({
   defaultOptions: {
