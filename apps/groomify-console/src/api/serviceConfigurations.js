da9ba7f405b1183fe6ebbb347e5faddcd4baa37a
KL <>
2026-03-19 14:28:24 -0400

    add getConfigByFKs apis

--- apps/groomify-console/src/api/serviceConfigurations.js
+++ apps/groomify-console/src/api/serviceConfigurations.js
@@ -3,3 +3,7 @@ import { apiFetch } from "./api";
 export async function getServiceConfigurations() {
   return await apiFetch("/api/serviceConfigurations");
 }
+
+export async function getConfigByFKs(serviceId, breedId, weightClassId ){
+  return await apiFetch(`/api/serviceConfigurations?service_id=${serviceId}&breed_id=${breedId}&weight_class_id=${weightClassId}`)
+}
