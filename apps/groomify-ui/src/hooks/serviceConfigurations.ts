f13affddd0bea654c80228500e7f0054f62994e1
KL <>
2026-03-19 14:28:35 -0400

    add getConfigByFKs hook

--- apps/groomify-ui/src/hooks/serviceConfigurations.ts
+++ apps/groomify-ui/src/hooks/serviceConfigurations.ts
@@ -1,10 +1,19 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getServiceConfigurations } from "@/api/serviceConfigurations";
+import { getServiceConfigurations, getConfigByFKs } from "@/api/serviceConfigurations";
 import { SERVICE_CONFIGURATIONS_QUERY_KEY } from "@/constants";
 
 export function useServiceConfigurations() {
   return useQuery({
     queryKey: [SERVICE_CONFIGURATIONS_QUERY_KEY],
     queryFn: getServiceConfigurations
+  });
+}
+
+export function useConfigByFKs(data) {
+  const{ serviceId, breedId, weightClassId } = data;
+  return useQuery({
+    queryKey: [SERVICE_CONFIGURATIONS_QUERY_KEY, serviceId, breedId, weightClassId],
+    queryFn: () => getConfigByFKs(serviceId, breedId, weightClassId),
+    enabled: !!serviceId && !!breedId && !!weightClassId
   });
 }
