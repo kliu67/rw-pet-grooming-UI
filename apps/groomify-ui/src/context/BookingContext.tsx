7674f887f999c968584fd5e8bdd9e8b9f1fa5a14
KL <>
2026-03-21 12:40:58 -0400

    add interface fields

--- apps/groomify-ui/src/context/BookingContext.tsx
+++ apps/groomify-ui/src/context/BookingContext.tsx
@@ -2,20 +2,37 @@ import { createContext, useContext, useState, ReactNode } from 'react';
 
 export interface BookingData {
   service?: {
-    id: string;
+    id: number;
     name: string;
-    price: number;
-    duration: number;
+    base_price: string;
+    code: string;
+    description: string;
+    uuid: string;
   };
   date?: string;
   time?: string;
-  personalInfo?: {
-    firstName: string;
+  // personalInfo?: {
+  //   firstName: string;
+  //   lastName: string;
+  //   email: string;
+  //   phone: string;
+  //   notes?: string;
+  // };
+   firstName: string;
     lastName: string;
     email: string;
     phone: string;
-    notes?: string;
-  };
+    petName: string;
+    weightClass:{
+      id: number;
+      code: string;
+      label: string
+      weight_bounds: [number, number];
+    };
+    breed: {
+      id: number;
+      name: string;
+    }
 }
 
 interface BookingContextType {
@@ -27,7 +44,31 @@ interface BookingContextType {
 const BookingContext = createContext<BookingContextType | undefined>(undefined);
 
 export function BookingProvider({ children }: { children: ReactNode }) {
-  const [bookingData, setBookingData] = useState<BookingData>({});
+  const [bookingData, setBookingData] = useState<BookingData>({
+    firstName: 'Kai',
+    lastName: 'Liu',
+    phone: '1234567890',
+    email: 'derekkailiu@gmail.com',
+    petName: 'Lou',
+    service:{
+      base_price: "40.00",
+      code: "BATH_BRUSH",
+      description: "description",
+      id: 35,
+      name: "Bath&Brush",
+      uuid: "0855aa36-21b7-48ce-8625-252767cafe47"
+    },
+    weightClass:{
+      code: "LARGE",
+      id: 3,
+      label: "large",
+      weight_bounds: [41, 60]
+    },
+    breed:{
+      id:15,
+      name: "Alpine Spaniel"
+    }
+  });
 
   const updateBookingData = (data: Partial<BookingData>) => {
     setBookingData((prev) => ({ ...prev, ...data }));
